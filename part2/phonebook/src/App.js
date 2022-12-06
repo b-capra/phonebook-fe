0ca650e1358938b68eccdf594f99a937a9fcc2bb
b-capra <caprabmc@gmail.com>
2022-12-06 16:13:45 -0500

    Complete exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,9 +4,13 @@ import {useState} from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {
+      name: 'Arto Hellas',
+      number: '123-456-7890'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -14,15 +18,21 @@ const App = () => {
     if (checkForDuplicate()) return
       
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNum
     }
 
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNum('')
   }
 
   const handleNameInput = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumInput = (event) => {
+    setNewNum(event.target.value)
   }
 
   const checkForDuplicate = () => {
@@ -45,12 +55,9 @@ const App = () => {
     <div>
       <h1>Phonebook</h1>
       <form onSubmit={addPerson}>
-        <div>
-          Name: <input value={newName} onChange={handleNameInput} />
-        </div>
-        <div>
-          <button type='submit'>Add</button>
-        </div>
+        <div>Name: <input value={newName} onChange={handleNameInput} /></div>
+        <div>Number: <input value={newNum} onChange={handleNumInput} /></div>
+        <div><button type='submit'>Add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
