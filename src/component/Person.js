fafb48bde95f48874c38e4c46157b703b4ab9479
b-capra <caprabmc@gmail.com>
2023-01-20 14:46:26 -0500

    Reenable delete button

--- src/component/Person.js
+++ src/component/Person.js
@@ -2,12 +2,7 @@ const Person = ({person, remove}) => {
   return (
     <li>
       {person.name}: {person.number}
-      <button
-        style={{display: "none"}} // Temporarily disables delete function
-        onClick={remove}
-      >
-        Remove
-      </button>
+      <button onClick={remove}>Remove</button>
     </li>
   )
 }
